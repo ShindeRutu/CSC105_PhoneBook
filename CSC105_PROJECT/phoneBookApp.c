f76ab1e49b66787f566eb4e58e2f162a1bf94b6a
Dinesh Gunaji Gawas <90246041+Dineshgawas64@users.noreply.github.com>
2021-11-25 04:29:45 +0000

    added some content in main.c

--- CSC105_PROJECT/phoneBookApp.c
+++ CSC105_PROJECT/phoneBookApp.c
@@ -18,6 +18,11 @@ int main()
   //system("color 4E");    //change console  color to black with greeen text (only fr windows platform)
   
   printf("\n***********PhoneBook**********\n\n");
+ // do{
+ printf("1-# CREATE_PHONEBOOK # \t 2-# INSERT_CONTACT # \t 3-# SHOW_CONTACT #\t 4-# EDIT_CONTACT # \n\n 5-# CALL \t 6-# RECENT_CALLS \t 7-# FAVOURITES \t 8-# DELETE_CONTACT \t 9-# DELETE_ALL_CONTACTS");
+ printf("\nEnter your choice: ");
+  //  scanf("%d",&choice);
+ //switch(choice){
   
   return 0;
 }
