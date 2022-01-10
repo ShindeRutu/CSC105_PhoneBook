1896df480e1e2d8101035fc6983d46503a7d7c4c
sp978077 <mca.2122@unigoa.ac.in>
2022-01-10 15:15:52 +0530

    ..

--- CSC105_PROJECT/phoneBook.h
+++ CSC105_PROJECT/phoneBook.h
@@ -5,92 +5,97 @@
     Program for : PhoneBook
     DS used : Stack, Queue, BST
     Date created :  November 2021 
-*/
-#include <stdio.h>
+*/ 
+
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
 
-typedef char keyType[20]; // fr search key
+  typedef char keyType[20];  // fr search key
+   
+  //to store mandatory info of the person
+  struct info
+  {
+    keyType key;        // name of the person
+    char companyName;   
+    int  mobNumber;  //contact_num : mobile
+    int  TelNumber;  //contact num : telephone(optional)
+    char email[20];  // email_add of contact
+	int fav;  
+	int call;  
+  };
+  typedef struct info info;
+  
 
-//to store mandatory info of the person
-struct info
-{
-  keyType key; // name of the person
-  char companyName;
-  long long int mobNumber; //contact_num : mobile
-  long long int TelNumber; //contact num : telephone(optional)
-  char email[20];          // email_add of contact
-  int fav;
-  int call;
-};
-typedef struct info info;
+  //to store additional info of the person (optional case)
+  struct additionalInfo
+  { 
+    int  wrkContact[10];  // contact of work place   
+    char nickName;      // nick name of the person
+    char dob[10];       //birth date : format = day-month-yr 
+    char relationship[10];  // realtion with contact
+    char website[40];     // web add 
+  };
+  typedef struct additionalInfo addInfo;
+   
 
-//to store additional info of the person (optional case)
-struct additionalInfo
-{
-  int wrkContact[10];    // contact of work place
-  char nickName;         // nick name of the person
-  char dob[10];          //birth date : format = day-month-yr
-  char relationship[10]; // realtion with contact
-  char website[40];      // web add
-};
-typedef struct additionalInfo addInfo;
+  // to point previous,next contact
+  struct one_contact        //node
+  {
+   	info data;
+   	struct one_contact *left;    //point to the previous contact
+    struct one_contact *right;    //point to the next contact
+  };
+  typedef struct one_contact node;
 
-// to point previous,next contact
-struct one_contact //node
-{
-  info data;
-  struct one_contact *left;  //point to the previous contact
-  struct one_contact *right; //point to the next contact
-};
-typedef struct one_contact node;
 
-// contact list tree
-struct contactBook //tree
-{
-  struct one_contact *root; // first contact
-  int count;                // to keep the count of contacts
-};
-typedef struct contactBook conBook;
+  // contact list tree
+  struct contactBook //tree
+  {
+	  struct one_contact *root;    // first contact
+	  int count;                   // to keep the count of contacts
+  };
+  typedef struct contactBook conBook;
 
-//to check if list is empty
-_Bool contactlist_isEmpty(conBook *cList);
 
-// to create the contact list once the program runs
-conBook *createContactList();
+  //to check if list is empty
+  _Bool contactlist_isEmpty(conBook *cList);
+  
+  // to create the contact list once the program runs
+  conBook* createContactList();
+  
+  // to add new contact to the list
+  bool newContact(conBook *cList , info data);
+  node* _insert(node *root, node *newPtr);
 
-// to add new contact to the list
-bool newContact(conBook *cList, info data);
-node *_insert(node *root, node *newPtr);
+  // display contact list
+  void displayContactList(conBook *cList);
+  
+  void _display_contactlist(node *root);    // inorder trav : recursive method
+  
+  // to delete the contact from the list
+  bool deleteContact(conBook *, keyType);
+  node* _delete(node *, keyType, bool *success);   //recursive method
+  
+  // to search the contact
+  node* search(node *root,keyType serKeyName);
 
-// display contact list
-void displayContactList(conBook *cList);
+  bool searchforinsert(node *root,keyType serKeyName);
+  
+  // to modify/edit the contact
+  _Bool modifyContact(conBook *cList ,keyType keyName, info data);
 
-void _display_contactlist(node *root); // inorder trav : recursive method
+  // display recent call list
+  _Bool recentCallLog(conBook *cList);
 
-// to delete the contact from the list
-bool deleteContact(conBook *, keyType);
-node *_delete(node *, keyType, bool *success); //recursive method
 
-// to search the contact
-node *search(node *root, keyType serKeyName);
+  // call contact 
+  node *call(node *root, keyType serkey);
 
-bool searchforinsert(node *root, keyType serKeyName);
+  // import/export contact
+void Export(conBook *cList, char file2[]);
+  //void BST_Destroy(contacts *tree);
 
-// to modify/edit the contact
-_Bool modifyContact(conBook *cList, keyType keyName, info data);
 
-// display recent call list
-_Bool recentCallLog(conBook *cList);
-
-// call contact
-node *call(node *root, keyType serkey);
-
-// import/export contact
-void Export(conBook *cList, FILE *wr);
-
-//void BST_Destroy(contacts *tree);
-
-node *del(node *, keyType value[]);
+  node* del(node* , keyType value[]);
 
 #endif
--- CSC105_PROJECT/queue.h
+++ CSC105_PROJECT/queue.h
@@ -4,75 +4,76 @@
                 DATE CREATED: SEPT 2021
                 DATE MODIFIED: SEPT 2021
  */
-#include <stdbool.h>
+#include<stdbool.h>
 #include "phoneBook.h"
 #ifndef _QUEUE_H
-#define _QUEUE_H
-typedef info eleType;
+    #define _QUEUE_H
+    typedef info eleType;
+    
+    //DATA
+    struct queue
+    {
+        struct queue_node *front;  //start
+        struct queue_node *rear;
+        int count;
+    };
+    typedef struct queue queueType;
 
-//DATA
-struct queue
-{
-    struct queue_node *front; //start
-    struct queue_node *rear;
-    int count;
-};
-typedef struct queue queueType;
+    struct queue_node
+    {
+        eleType data;
+        struct queue_node *next;
 
-struct queue_node
-{
-    eleType data;
-    struct queue_node *next;
-};
-typedef struct queue_node qnode;
+    };
+    typedef struct queue_node qnode;
 
 //prototypes
-
-// Function: removes element from front of the queue
-// Precondition: queue is not empty
-// Input: *queueType
-// output: element from the front queue
-// Postcondition: element at front is deleted
-eleType dequeue(queueType *s1); //remove from begining
-                                // Function: destroys the queue
-// Precondition: queue is created
-// Input: *queueType
-// output: none
-// Postcondition: deletes all elements and free memory
-void destroyQueue(queueType *);
-
-//Function :  creates a new empty queue
-//precondition : none
-//input: none
-//output: qType
-//postCondition : an Empty queue is created
-queueType createQueue();
-// Function: inserts an element on rear
-// Precondition: queue is not full
-// Input: *queueType, eleType
-// output: true if enque is successful and false if enque is not successful
-// Postcondition: new element is inserted
-_Bool enqueue(eleType ele, queueType *s1); //add at end
-
-// Function: checks if the queue is full
-// Precondition: none
-// Input:  queueType
-// output: true if queue is full and false if queue is not full
-// Postcondition: none
-_Bool isFull(queueType s);
-// Function: checks if the queue is empty
-// Precondition: none
-// Input:  queueType
-// output: true if queue is empty and false if queue is not empty
-// Postcondition: none
-_Bool isEmpty(queueType s);
-
-//Function: displays the elements of the queue
-//Precondition: queue is not empty
-//Input: queueType
-//output: displays the elements of the queue
-//Postcondition: none
-
-void display(queueType *);
-
+   
+    // Function: removes element from front of the queue
+    // Precondition: queue is not empty
+    // Input: *queueType
+    // output: element from the front queue
+    // Postcondition: element at front is deleted
+    eleType dequeue(queueType *s1);//remove from begining
+     // Function: destroys the queue
+    // Precondition: queue is created
+    // Input: *queueType
+    // output: none
+    // Postcondition: deletes all elements and free memory
+    void destroyQueue(queueType *);
+    
+     //Function :  creates a new empty queue
+    //precondition : none
+    //input: none
+    //output: qType
+    //postCondition : an Empty queue is created
+    queueType createQueue();
+    // Function: inserts an element on rear
+    // Precondition: queue is not full
+    // Input: *queueType, eleType
+    // output: true if enque is successful and false if enque is not successful
+    // Postcondition: new element is inserted
+    _Bool enqueue(eleType ele, queueType *s1);//add at end
+    
+     // Function: checks if the queue is full
+    // Precondition: none
+    // Input:  queueType
+    // output: true if queue is full and false if queue is not full
+    // Postcondition: none
+    _Bool isFull(queueType s);
+     // Function: checks if the queue is empty
+    // Precondition: none
+    // Input:  queueType
+    // output: true if queue is empty and false if queue is not empty
+    // Postcondition: none
+    _Bool isEmpty(queueType s);
+    
+    //Function: displays the elements of the queue
+    //Precondition: queue is not empty
+    //Input: queueType
+    //output: displays the elements of the queue
+    //Postcondition: none
+    
+    void display(queueType *);
+    
 #endif
